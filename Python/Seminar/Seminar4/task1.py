60a2756f0811da9e399822ad2b44554934a101f7
PochechuevAE <140548126+PochechuevAE@users.noreply.github.com>
2023-10-20 21:08:14 +0300

    Add files via upload

---
+++ Python/Seminar/Seminar4/task1.py
@@ -0,0 +1,27 @@
+sp = [88, 77, True, 9.99, "Hello", "world"]
+sp2 = [1, 2]
+# print(sp + sp2)
+
+# for item in sp:
+#     print(item)
+    
+# for i in range (len(sp)):
+#     print(sp[i])
+
+# for i, value in enumerate(sp): # нумерует
+#     print(i, value)
+
+# d = {"Ваня": 89529967898, "Вася": 895299}
+# d["Сергей"] = 11111
+# print(d)
+# print(d.keys())
+# for key, value in d.items():
+#     print(key, value)
+
+s = {77, 777, 777, 777, 77,1,2,3}
+print(s)
+s.add(1000)
+print(s)
+s.discard(1)
+print(s)
+s.discard(11)
---
+++ Python/Seminar/Seminar4/task4.py
@@ -0,0 +1,18 @@
+""" 
+Напишите программу для печати всех уникальных
+значений в словаре.
+Input: [{"V": "S001"}, {"V": "S002"}, {"VI": "S001"},
+{"VI": "S005"}, {"VII": " S005 "}, {" V ":" S009 "}, {" VIII
+":" S007 "}]
+Output: {'S005', 'S002', 'S007', 'S001', 'S009'}
+
+"""
+
+data = [{"V": "S001"}, {"V": "S002"}, {"VI": "S001"}, {"VI": "S005"}, {"VII": " S005 "}, {" V ":" S009 "}, {" VIII ":" S007 "}]
+unique_values = set()
+
+for item in data:
+    for value in item.values():
+        unique_values.add(value.strip())  # Удаляем лишние пробелы и добавляем в множество
+
+print(unique_values)
